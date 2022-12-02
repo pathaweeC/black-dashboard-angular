5d93a075d98552e907b0044a0ad748b2aceda7b9
Paan <pathaweechayakul@gmail.com>
2022-12-02 17:01:39 +0700

    edit Yaxis at main dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -155,8 +155,8 @@ export class DashboardComponent implements OnInit {
             zeroLineColor: "transparent",
           },
           ticks: {
-            suggestedMin: 60,
-            suggestedMax: 125,
+            suggestedMin: Math.min(...this.vac)-5,
+            suggestedMax: Math.max(...this.vac)+5,
             padding: 20,
             fontColor: "#9a9a9a"
           }
@@ -300,8 +300,8 @@ export class DashboardComponent implements OnInit {
             zeroLineColor: "transparent",
           },
           ticks: {
-            suggestedMin: 60,
-            suggestedMax: 120,
+            suggestedMin: Math.min(...this.vac)-5,
+            suggestedMax: Math.max(...this.vac)+5,
             padding: 20,
             fontColor: "#9e9e9e"
           }
@@ -436,49 +436,16 @@ export class DashboardComponent implements OnInit {
           pointHoverRadius: 4,
           pointHoverBorderWidth: 15,
           pointRadius: 4,
-          // data: this.data,
+          
+          Low: Math.min(...this.vac)-5,
+          High: Math.max(...this.vac)+5,
+      // data: this.data,
         }]
       },
-      low: Math.min(...this.vac)-5,
-      high: Math.max(...this.vac)+5,
+
       options: gradientChartOptionsConfigurationWithTooltipRed,
     };
     this.myChartData = new Chart(this.ctx, config);
-
-////////////////////////////////////
-
-    this.canvas = document.getElementById("CountryChart");
-    this.ctx  = this.canvas.getContext("2d");
-    var gradientStroke = this.ctx.createLinearGradient(0, 230, 0, 50);
-
-    gradientStroke.addColorStop(1, 'rgba(29,140,248,0.2)');
-    gradientStroke.addColorStop(0.4, 'rgba(29,140,248,0.0)');
-    gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
-
-    var myChart = new Chart(this.ctx, {
-      type: 'bar',
-      responsive: true,
-      legend: {
-        display: false
-      },
-      data: {
-        labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
-        datasets: [{
-          label: "Countries",
-          fill: true,
-          backgroundColor: gradientStroke,
-          hoverBackgroundColor: gradientStroke,
-          borderColor: '#1f8ef1',
-          borderWidth: 2,
-          borderDash: [],
-          borderDashOffset: 0.0,
-          data: [53, 20, 10, 80, 100, 45],
-        }]
-      },
-      options: gradientBarChartConfiguration
-    });
-
-///////////////////////////////////////////////
 
   }
   public updateOptions() {
